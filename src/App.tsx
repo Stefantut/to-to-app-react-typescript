51985195d23c11a55ee515ac17574f772c78a851
Stefan Tutuianu <dragos_tutuianu@yahoo.com>
2022-01-18 22:54:49 +0000

    Added all html elements and styled them

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,19 @@ import React, { FC } from "react";
 import "./App.css";
 
 const App: FC = () => {
-  return <div className="App">app</div>;
+  return (
+    <div className="App">
+      <h1>To do App</h1>
+      <header>
+        <div className="inputs-container">
+          <input type="text" placeholder="Task..." />
+          <input type="number" placeholder="Deadline (days)" />
+        </div>
+        <button>Add Task </button>
+      </header>
+      <div className="todoList"></div>
+    </div>
+  );
 };
 
 export default App;
